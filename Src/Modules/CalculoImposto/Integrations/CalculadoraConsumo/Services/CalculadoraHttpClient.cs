09f24938abeac687121f54afb76d6bb55e813464
Ruan <ruanalmeidawegner@gmail.com>
2025-08-09 20:47:04 -0300

    feat Modulo CalculoImposto: Iniciada integração com API de Tributação sobre Consumo.

---
+++ Src/Modules/CalculoImposto/Integrations/CalculadoraConsumo/Services/CalculadoraHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CalculoImposto.Integrations.CalculadoraConsumo.Services
+{
+    internal class CalculadoraHttpClient : ICalculadoraHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+    }
+}
